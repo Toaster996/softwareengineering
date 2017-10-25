0a102b1aad4d000b87a447197a1730188e2df662
Philipp7501 <32509406+Philipp7501@users.noreply.github.com>
2017-10-25 09:37:15 +0200

    Created UI Prototype

--- DigitalJournal/src/main/java/de/dhbw/softwareengineering/controller/IndexController.java
+++ DigitalJournal/src/main/java/de/dhbw/softwareengineering/controller/IndexController.java
@@ -1,8 +1,11 @@
 package de.dhbw.softwareengineering.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * Created by saurabh on 2/11/17.
@@ -10,9 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index(Model m) {
-        m.addAttribute("someAttribute", "someValue");
-        return "index";
+    @RequestMapping("/**")
+    public void index(HttpServletRequest request, HttpServletResponse response) {
+
+
+        String filename = request.getRequestURL().toString().replace("http://localhost:8080/", "");
+
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        File file = new File(classLoader.getResource("html/" + filename).getFile());
+
+            try {
+                // get your file as InputStream
+                InputStream is = new FileInputStream(file);
+                // copy it to response's OutputStream
+                org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+                response.flushBuffer();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
     }
 }
--- DigitalJournal/src/main/webapp/WEB-INF/views/index.jsp
+++ DigitalJournal/src/main/webapp/WEB-INF/views/index.jsp
@@ -5,13 +5,4 @@
   Time: 08:05
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  <p>${someAttribute}</p>
-  </body>
-</html>
+${someAttribute}
