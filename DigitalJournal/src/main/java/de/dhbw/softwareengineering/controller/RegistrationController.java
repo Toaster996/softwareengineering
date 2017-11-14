790cfedcfa3e58528684e2d41ff3bfda802efbbb
Philipp7501 <32509406+Philipp7501@users.noreply.github.com>
2017-11-14 23:00:15 +0100

    Registration UC

--- DigitalJournal/src/main/java/de/dhbw/softwareengineering/controller/RegistrationController.java
+++ DigitalJournal/src/main/java/de/dhbw/softwareengineering/controller/RegistrationController.java
@@ -1,7 +1,8 @@
 package de.dhbw.softwareengineering.controller;
 
-import de.dhbw.softwareengineering.model.Employee;
 import de.dhbw.softwareengineering.model.User;
+import de.dhbw.softwareengineering.utilities.MySQL;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +28,7 @@ public class RegistrationController {
     }
 
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
-    public String submit(@Valid @ModelAttribute("employee") final User user, final BindingResult result, final ModelMap model) {
+    public String submit(@Valid @ModelAttribute("user") final User user, final BindingResult result, final ModelMap model) {
         if (result.hasErrors())
             return "error";
 
@@ -34,6 +38,25 @@ public class RegistrationController {
         model.addAttribute("password", user.getPassword());
         UserMap.put(user.getName(), user);
         System.out.println(user);
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        MySQL mySQL = MySQL.getInstance();
+
+        Connection connection = mySQL.getConnection();
+
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement("INSERT INTO 'users' ('username', 'email', 'password','registrationDate') VALUES(?,?,?,?);");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, bCryptPasswordEncoder.encode(user.getPassword()));
+            preparedStatement.setLong(4, System.currentTimeMillis());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return "info";
 
     }
