37cd968ffcce565c71e5209719db2c8d7343f2c0
Florian Straub <straub.flo@outlook.de>
2017-12-08 10:55:44 +0100

    Polishing recover password
    ^DJ-59

--- DigitalJournal/src/main/java/de/dhbw/softwareengineering/utilities/Constants.java
+++ DigitalJournal/src/main/java/de/dhbw/softwareengineering/utilities/Constants.java
@@ -9,10 +9,15 @@ public class Constants {
     public static final ClassPathXmlApplicationContext applicationContext;
     public static PrettyPrinter prettyPrinter = new PrettyPrinter();
 
+
     static{
         ApplicationContextProvider appContext = new ApplicationContextProvider();
         applicationContext = new ClassPathXmlApplicationContext(appContext.getApplicationContext());
     }
+
+    public static final String SESSION_CHANGEPWUSER            = "changePasswordUser";
+    public static final String SESSION_LOGGEDINUSER            = "loggedInUser";
+    public static final String SESSION_CURRENTJOURNAL          = "currentJournal";
 
     public final static String STATUS_ATTRIBUTE_NAME           = "status";
     public final static String STATUSCODE_PWMISSMATCH          = "pwmissmatch";
@@ -25,10 +30,10 @@ public class Constants {
     public final static String STATUSCODE_EMAILINVALID         = "emailinvalid";
     public final static String STATUSCODE_EMAILALREADYINUSE    = "usedemail";
     public final static String STATUSCODE_USERNAMEALREADYINUSE = "useduser";
-    public final static String STATUSCODE_MODAL_TEMP = "temp_modal";
-
-    public final static String STATUSCODE_MODAL_HEADER = "temp_modal_header";
-    public final static String STATUSCODE_MODAL_BODY = "temp_modal_header";
+    public final static String STATUSCODE_MODAL_TEMP           = "temp_modal";
+    public final static String STATUSCODE_PWCHANGESUCCESS      = "pwchangesuccess";
+    public final static String STATUSCODE_MODAL_HEADER         = "temp_modal_header";
+    public final static String STATUSCODE_MODAL_BODY           = "temp_modal_header";
 
     public final static String CONFIGURATION_DIRECTORY = "." + File.separator + "conf";
     public final static String MYSQL_CONFIGURATION_NAME = "mysql.conf";
@@ -41,7 +46,7 @@ public class Constants {
 
     public static final String[] SUPPORT_RECIPIENT = new String[]{"minerlevel@gmx.de"};
 
-    public static int ONE_HOUR_IN_MILLIS = 24 * 60 * 60 * 1000;
-    public static int ONE_DAY_IN_MILLIS = 24 * ONE_HOUR_IN_MILLIS;
+    public static final int ONE_HOUR_IN_MILLIS = 24 * 60 * 60 * 1000;
+    public static final int ONE_DAY_IN_MILLIS = 24 * ONE_HOUR_IN_MILLIS;
 
 }
