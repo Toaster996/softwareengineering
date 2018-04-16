62bf8645c0c2880b6091d50ee0110f5ffb4d0430
Toaster996 <32509408+Toaster996@users.noreply.github.com>
2018-04-16 21:02:27 +0200

    Edit Goal working DJ-96

--- DigitalJournal/src/main/java/de/dhbw/softwareengineering/digitaljournal/controller/GoalController.java
+++ DigitalJournal/src/main/java/de/dhbw/softwareengineering/digitaljournal/controller/GoalController.java
@@ -4,29 +4,19 @@ import de.dhbw.softwareengineering.digitaljournal.business.GoalService;
 import de.dhbw.softwareengineering.digitaljournal.domain.ContactRequest;
 import de.dhbw.softwareengineering.digitaljournal.domain.Goal;
 import de.dhbw.softwareengineering.digitaljournal.domain.form.CreateGoal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-
 import java.security.Principal;
 
-import static de.dhbw.softwareengineering.digitaljournal.util.Constants.STATUSCODE_EMPTYFORM;
-import static de.dhbw.softwareengineering.digitaljournal.util.Constants.STATUSCODE_MODAL_BODY;
-import static de.dhbw.softwareengineering.digitaljournal.util.Constants.STATUSCODE_MODAL_HEADER;
-import static de.dhbw.softwareengineering.digitaljournal.util.Constants.STATUSCODE_MODAL_TEMP;
-import static de.dhbw.softwareengineering.digitaljournal.util.Constants.STATUS_ATTRIBUTE_NAME;
+import static de.dhbw.softwareengineering.digitaljournal.util.Constants.*;
 
+@Slf4j
 @Controller
 @RequestMapping("/journal/goal")
 public class GoalController {
@@ -66,14 +56,29 @@ public class GoalController {
 
     @PostMapping("/delete/{goalId}")
     public String delete(@PathVariable String goalId, Model model, Principal principal) {
-        model.addAttribute("contactRequest",new ContactRequest());
-
         Goal goal = goalService.getById(goalId);
 
         if (goal.getUsername().equals(principal.getName())) {
             goalService.deleteById(goalId);
         }
 
+        return "redirect:/journal";
+    }
+
+    @GetMapping("/edit/{goalId}")
+    public String editGoal(@PathVariable String goalId, Model model, RedirectAttributes redir, Principal principal) {
+        Goal goal = goalService.getById(goalId);
+
+        if (goal.getUsername().equals(principal.getName())) {
+            redir.addFlashAttribute(STATUS_ATTRIBUTE_NAME, "editGoal");
+            redir.addFlashAttribute("editGoal", goal);
+        }
+        return "redirect:/journal";
+    }
+
+    @PostMapping("/edit")
+    public String editGoal(@Valid @ModelAttribute("goal") final CreateGoal goal, Model model, Principal principal) {
+        System.out.println(goal.getName());
         return "redirect:/journal";
     }
 
